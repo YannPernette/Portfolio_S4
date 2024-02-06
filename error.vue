561746903e9161835e88cecb16dd6868eab2d4da
YannPernette <yann.pernette@edu.univ-fcomte.fr>
2024-02-06 15:05:15 +0100

    commencement page erreur

---
+++ error.vue
@@ -0,0 +1,10 @@
+<template>
+  <CustomCursor />
+  <div class="errorParent absolute z-[-1]">
+    <h1 title="404" class="errorDiv">404</h1>
+  </div>
+
+  <div class="absolute top-1/2">
+    <h2>Page non trouvée</h2>
+  </div>
+</template>
